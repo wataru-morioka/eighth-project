99ee3146abbc5ce6b8c4228456e7243768748532
WataruMorioka <wtr_morioka@icloud.com>
2019-07-30 08:24:18 +0900

    service worker実装開始

--- src/run.py
+++ src/run.py
@@ -7,16 +7,21 @@ from flask_migrate import Migrate
 from flask_marshmallow import Marshmallow
 from sqlalchemy import func
 import os
+import requests
+import json
+import logging
+from logging import getLogger 
+logger = getLogger(__name__)
 
-# POSTGRES_URL = os.environ["POSTGRES_URL"]
-# POSTGRES_USER = os.environ["POSTGRES_USER"]
-# POSTGRES_PASSWORD = os.environ["POSTGRES_PASSWORD"]
-# POSTGRES_DB = os.environ["POSTGRES_DB"]
+# POSTGRES_URL = os.environ['POSTGRES_URL']
+# POSTGRES_USER = os.environ['POSTGRES_USER']
+# POSTGRES_PASSWORD = os.environ['POSTGRES_PASSWORD']
+# POSTGRES_DB = os.environ['POSTGRES_DB']
 
-MYSQL_URL = os.environ["MYSQL_URL"]
-MYSQL_USER = os.environ["MYSQL_USER"]
-MYSQL_PASSWORD = os.environ["MYSQL_PASSWORD"]
-MYSQL_DB = os.environ["MYSQL_DB"]
+MYSQL_URL = os.environ['MYSQL_URL']
+MYSQL_USER = os.environ['MYSQL_USER']
+MYSQL_PASSWORD = os.environ['MYSQL_PASSWORD']
+MYSQL_DB = os.environ['MYSQL_DB']
 
 # DB_URL = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(
 #     user=POSTGRES_USER,
@@ -43,9 +48,9 @@ api = Api(app)
 
 # テストデータ
 users = [
-    { "id": "U001", "name": "ユーザ太郎", "age": 27 },
-    { "id": "U002", "name": "ユーザ二郎", "age": 20 },
-    { "id": "U003", "name": "ユーザ三郎", "age": 10 }
+    { 'id': 'U001', 'name': 'ユーザ太郎', 'age': 27 },
+    { 'id': 'U002', 'name': 'ユーザ二郎', 'age': 20 },
+    { 'id': 'U003', 'name': 'ユーザ三郎', 'age': 10 }
 ]
 
 class Users(db.Model):
@@ -67,11 +72,8 @@ users_schema = UsersSchema(many=True)
 
 class UserInfo(Resource):
     def get(self):
-        """
-        ユーザを１件取得する
-        """
         # id = request.args.get('id')
-        # result = [n for n in users if n["id"] == id]
+        # result = [n for n in users if n['id'] == id]
 
         # user = User(1, 'wataru')
 
@@ -91,25 +93,29 @@ class UserInfo(Resource):
         #     abort(404)
 
     def post(self):
-        """
-        ユーザを登録する
-        """
         #ユーザを追加
-        users.append(request.json)
+        resp = requests.post('https://www.googleapis.com/identitytoolkit/v3/'
+                                      'relyingparty/getAccountInfo'
+                                      '?key={}'.format('AIzaSyBb1EQB5F7Q7O9n8BH1Fy929XhH7tRy6OM'),
+                                      params={'idToken': request.json['idToken']})
+        logger.debug("userinfo:{}".format(resp.text))
+        print(resp.text)
+        data = json.loads(resp.text)
+        body = {
+            'uid': data['users'][0]['localId'],
+            'email': data['users'][0]['email'],
+            'displayName': data['users'][0]['displayName'],
+        }
 
-        #正常に登録できたので、HTTP status=204(NO CONTENT)を返す
-        return '', 204
+        return jsonify(body)
 
     def put(self):
-        """
-        ユーザを更新する
-        """
         user = request.json
-        lst = [val for val in users if val["id"] == user["id"]]
+        lst = [val for val in users if val['id'] == user['id']]
         
         if len(lst) >= 1: 
-            lst[0]["name"] = user["name"]
-            lst[0]["age"] = user["age"]
+            lst[0]['name'] = user['name']
+            lst[0]['age'] = user['age']
         else:
             #存在しないユーザIDが指定された場合
             abort(404)
@@ -118,11 +124,8 @@ class UserInfo(Resource):
         return '', 204
 
     def delete(self):
-        """
-        ユーザを削除する
-        """
         id = request.args.get('id')
-        lst = [i for i, val in enumerate(users) if val["id"] == id]
+        lst = [i for i, val in enumerate(users) if val['id'] == id]
         for index in lst:
             del users[index]
 
@@ -135,5 +138,20 @@ class UserInfo(Resource):
 
 api.add_resource(UserInfo, '/user')
 
-if __name__ == "__main__":
+def setLogger():
+    #handler1を作成
+    handler1 = logging.StreamHandler()
+    handler1.setFormatter(logging.Formatter("%(asctime)s %(levelname)8s %(message)s"))
+
+    #handler2を作成
+    handler2 = logging.FileHandler(filename="test.log")  #handler2はファイル出力
+    handler2.setLevel(logging.WARN)     #handler2はLevel.WARN以上
+    handler2.setFormatter(logging.Formatter("%(asctime)s %(levelname)8s %(message)s"))
+
+    #loggerに2つのハンドラを設定
+    logger.addHandler(handler1)
+    logger.addHandler(handler2)
+
+if __name__ == '__main__':
+    setLogger()
     app.run(debug=True)
